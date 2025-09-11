a0893b4d9f4c0a3e5940298da146f3946bef689f
Sandeeep Sahoo <118088735+sandy-0911@users.noreply.github.com>
2025-09-11 12:59:12 +0000

    database names changes

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         console.log("Database Connected");
     })
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/ai-image`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
 
 }
 
