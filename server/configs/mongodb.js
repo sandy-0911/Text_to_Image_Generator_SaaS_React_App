a39ea537581605f438d91a59cc67a18773bc75c6
Sandeeep Sahoo <118088735+sandy-0911@users.noreply.github.com>
2025-09-11 13:13:25 +0000

    minor changes

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -6,7 +6,8 @@ const connectDB = async () => {
         console.log("Database Connected");
     })
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
+
 
 }
 
